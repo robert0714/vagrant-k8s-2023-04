dc028b26bd7a4cb53d5ebd4d6ce341abd7c45acf
robert0714 <robert0714@gmail.com>
2023-04-27 08:40:15 +0800

    add parameter for specific k8s version

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+KUBE_VERSION=1.24.13-00
 
 sudo apt-get update
 sudo apt-get install --quiet --yes apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install --quiet --yes kubelet kubeadm kubectl
+sudo apt-get install --quiet --yes kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
 sudo apt-mark hold kubelet kubeadm kubectl
